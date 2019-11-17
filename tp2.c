1ac7dd2ca284d39443c59a5eccc4aa185e798209
CerrutiLautaro <your_email_address@example.com>
2019-11-16 22:43:28 -0300

    Fixed a few things

--- tp2.c
+++ tp2.c
@@ -11,15 +11,19 @@ typedef struct {
   char Nombre[40], Apellido[40], Edad[3];
   int Codigo_localidad, Genero, Genero_Interes;
 }Persona;
+
 /*
-  ComparaInt 
+  ComparaInt
+  Toma dos ints a y b, y devuelve un numero mayor a 0 si a es mayor a b
+  0 si son iguales, y un numero menor a 0 si b es mayor a a
 */
 int comparaInt(const void *i, const void *j){
   int *a = (int *) i, *b = (int *)j;
   return *a - *b;
 }
+
 /*
-  swapint Toma un array de enteros, una posicion1 y otra posicion e intercambia de lugar el numero en la 
+  swapint Toma un array de enteros, una posicion y otra posicion e intercambia de lugar el numero en la 
 primera posicion con el de la segunda
 */
 void swapint(int* numeros,int posicion1,int posicion2){
@@ -27,22 +31,25 @@ void swapint(int* numeros,int posicion1,int posicion2){
   numeros[posicion1] = numeros[posicion2];
   numeros[posicion2] = buffer;
 }
+
 /*
-  Genenra una lista de n numeros randoms no repetidos, siendo n un numero que se ingreso como parametro de la funcion
+  Genera una lista de n numeros randoms no repetidos, siendo n un numero que se ingreso como parametro de la funcion
+  Toma el numero maximo que puede generar, la cantidad de numeros randoms a generar, y un puntero a un array de donde se almacenara la lista
 */
-void GeneradorNnumerosRandoms(int lineasArchivo, int cantdidatos, int* resultado){
+void GeneradorNnumerosRandoms(int lineasArchivo, int CantidadPersonas, int* resultado){
   int *Randoms = (int*) malloc(sizeof(int)*lineasArchivo), i, aleatorio;
 
   for( i = 0;i<lineasArchivo;i++){
     Randoms[i] = i;
   }
-  for(i = 0 ; i < cantdidatos; i++){
+  for(i = 0 ; i < CantidadPersonas; i++){
     aleatorio = rand() % (lineasArchivo  - i) ;
     resultado[i] = Randoms[aleatorio];
     swapint(Randoms, aleatorio, (lineasArchivo-i-1));
   }
   free(Randoms);
 }
+
 /*
 Dado un archivo cuenta la cantidad de lineas que tiene el archivo dado
 ContadorDeLineas: File -> Int
@@ -56,16 +63,17 @@ int ContadorDeLineas(FILE* Entrada){
   rewind(Entrada);
   return lineasArchivoPersonas;
 }
+
 /*
-  Dado un archivo con personas, la cantidad de lineas del archivo, un array de la estructura antes descripta y un numero n
-  guarda en el array de la estructura, n personas random no repetidas del archivo.
+  Dado un la cantidad de personas, un archivo de personas, la cantidad de lineas de este archivo y un array de la estructura antes descripta
+  guarda en el array de la estructura, n personas random no repetidas, sacadas del archivo.
 */
-void obtencion_de_personas (int cantdidatos, FILE* Entrada, int lineasArchivoPersonas, Persona *personas){
-  int *NumerosDePersonas = (int*) malloc(sizeof(int)*cantdidatos), iterador, PersonaBuscada = 0, iteradorArchivo = -1, codigoLocalidad, genero, generoInteres;
+void obtencion_de_personas (int CantidadPersonas, FILE* Entrada, int lineasArchivoPersonas, Persona *personas){
+  int *NumerosDePersonas = (int*) malloc(sizeof(int)*CantidadPersonas), iterador, PersonaBuscada = 0, iteradorArchivo = -1, codigoLocalidad, genero, generoInteres;
   char nombre[40], apellido[40], edad[3];
-  GeneradorNnumerosRandoms(lineasArchivoPersonas, cantdidatos, NumerosDePersonas);
-  qsort(NumerosDePersonas, cantdidatos, sizeof(int), comparaInt);
-  for(iterador = 0; iterador < cantdidatos; ++iterador){
+  GeneradorNnumerosRandoms(lineasArchivoPersonas, CantidadPersonas, NumerosDePersonas);
+  qsort(NumerosDePersonas, CantidadPersonas, sizeof(int), comparaInt);
+  for(iterador = 0; iterador < CantidadPersonas; ++iterador){
     PersonaBuscada = NumerosDePersonas[iterador];
     for(; iteradorArchivo != PersonaBuscada; iteradorArchivo++){
       fscanf(Entrada, "%[^,],%[^,],%d,%[^,],%d,%d", nombre, apellido, &codigoLocalidad, edad, &genero, &generoInteres);
@@ -79,8 +87,9 @@ void obtencion_de_personas (int cantdidatos, FILE* Entrada, int lineasArchivoPer
     personas[iterador].Genero_Interes = generoInteres;
   }
 }
+
 /*
-  Dado un string, regularisa el strings, es decir quita los espacios de mas del string
+  Dado un string, formatea el strings, es decir quita los espacios de mas del string
 */
 void formateo(char* string){
   int iterador, bandera = 1;
@@ -91,8 +100,9 @@ void formateo(char* string){
     }
   }
 }
+
 /*
-  Toma un archivo de entrada con las localidades,la cantidad de lineas del archivo y un array de strings
+  Toma un archivo de entrada con las localidades, la cantidad de lineas del archivo y un array de strings
   y guarda en el array todas las localidades de forma tal que el codigo de la ciudad corresponde con el indice del array
 */
 void CargaDeLocalidades (FILE* Ciudades, int lineasArchivoLocalidades, char** localidades){
@@ -106,20 +116,21 @@ void CargaDeLocalidades (FILE* Ciudades, int lineasArchivoLocalidades, char** lo
     strcpy(localidades[index], localidad);
   }
 }
+
 /*
-recive un archivo de salida, la cantidad de personas a imprimir, un array con la estructura que representa a las personas, las localidades
+recive un archivo de salida, la cantidad de personas a imprimir, un array de la estructura que representa a las personas, las localidades
 un array con los posibles generos y otro array con los posibles generos de interes, luego toma los datos de las personas y los imprime en 
 el archivo de salida
 */
-void ImprimePersonas(FILE* Salida, int cantdidatos, Persona *personas, char** localidades, char *genero, char *generointeres){
+void ImprimePersonas(FILE* Salida, int CantidadPersonas, Persona *personas, char** localidades, char *genero, char *generointeres){
   int i = 0;
-  for(i = 0; i < cantdidatos; ++i){
+  for(i = 0; i < CantidadPersonas; ++i){
     fprintf(Salida, "%s,%s,%s,%s,%c,%c\n", personas[i].Nombre, personas[i].Apellido, localidades[personas[i].Codigo_localidad], personas[i].Edad, genero[personas[i].Genero-1], generointeres[personas[i].Genero_Interes-1]);
   }
 }
 
 int main(){
-  int cantdidatos, lineasArchivoPersonas, lineasArchivoLocalidades, iterador;
+  int CantidadPersonas, lineasArchivoPersonas, lineasArchivoLocalidades, iterador;
   FILE *Entrada, *Ciudades, *Salida;
   Persona *personas;
   char **localidades, *genero="MF", *generointeres="FMAN";
@@ -131,15 +142,20 @@ int main(){
   localidades = (char**) malloc(sizeof(char*)*lineasArchivoLocalidades);
 
   do{
-    scanf("%d",&cantdidatos);
-  } while(cantdidatos > lineasArchivoPersonas);
-  personas = (Persona*) malloc(sizeof(Persona)*cantdidatos);
-  obtencion_de_personas(cantdidatos, Entrada, lineasArchivoPersonas, personas);
+    printf("Ingrese la cantidad de personas: ");
+    if(CantidadPersonas > lineasArchivoPersonas){
+      printf("El numero ingresado supera las personas disponibles, ingreselo nuevamente.\n");
+    }
+    scanf("%d",&CantidadPersonas);
+  } while(CantidadPersonas > lineasArchivoPersonas);
+  personas = (Persona*) malloc(sizeof(Persona)*CantidadPersonas);
+  obtencion_de_personas(CantidadPersonas, Entrada, lineasArchivoPersonas, personas);
   fclose(Entrada);
   CargaDeLocalidades(Ciudades, lineasArchivoLocalidades, localidades);
   fclose(Ciudades);
   Salida = fopen("salida.txt","w+");
-  ImprimePersonas(Salida, cantdidatos, personas, localidades, genero, generointeres);
+  ImprimePersonas(Salida, CantidadPersonas, personas, localidades, genero, generointeres);
+  fclose(Salida);
   free(localidades);
   free(personas);
   return 0;
